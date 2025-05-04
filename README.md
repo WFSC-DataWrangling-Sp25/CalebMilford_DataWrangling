52567c052a1524e6c2ff6ba8d579d3b5b5c179e5
Caleb Milford <cmilford@arizona.edu>
2025-05-03 23:58:50 -0700

    Uploadig the plannig file and updtaes to README

--- README.md
+++ README.md
@@ -6,12 +6,16 @@
 
 This repository includes data sets and code looking at the encounter history of Sonoran desert toad and Sonoran mud turtles over the course of 3 years. These are presence/ absence studies that also record different landscape and site covariate data. The code is designed to help determine what are detection level covariates for these two species.
 
-## Structure 
+## Structure
 
 -   data_raw - starting data
 
+    -   The data I am using primarily is in the "Combined_Frog" CSV file
+
 -   data_clean - data that has been restructures and/or cleaned
 
--   scripts - differnt code chunks
+-   scripts - different code chunks
+
+    -   Planning doc - just for me to help think about all the objectives I need to accomplish both between this class and what would be useful for my thesis.
 
 -   output - plots and graphs
---
+++ scripts/Planning_Script.R
@@ -0,0 +1,26 @@
+#Final Project Planning script 
+#Caleb Milford 
+#cmilford@arizona.edu
+#May the 4th be with you 
+
+#Guidelines; 
+  #10 weeks of total material: following are required (label each one)
+    #Week 5: Data visualization (ggplot) (there are literally a million things)
+    #Week 8: Basically just writing out instrictions of what youre doing in the code.
+    #Beginning description and discriptions between each code chunk 
+    #Week 9: Just need to put it in an R studio project aka donezo 
+    #Week 10: Needs to be in a "Final Project Github repo. Think this is done? Double check 
+    #Week 11: Functions: Write my own (something to do with site level covariates, maybe like total vegetation cover)
+    #Week 12: Making Choices: if/else statements (Can you filter target species like this?)
+    #Week 13: Iterations (loopy loops) (not sure, should ask Ellen)
+  
+  #Javan suggested tasks and how to apply them to weeks 
+    #General data clean up (Week 6?) DO THIS FIRST
+    #Looking at net surveys (Week 3?) 
+    #One row per survey with only my target species(Week 6?) DO THIS SECOND
+    #summarize detections by month/year (week 4?) Might need to ask Javan for clarification on this 
+    #summarize # of sites surveyed per year (simple data frame) (week 3?) PROBABLY CAN DO THIS THIRD
+
+
+#Actual first things that need to be done are confirming that weeks 8,9,10 are 
+#completed and set up properly. Once that is complete, then you can more on to "DO THIS FIRST"
